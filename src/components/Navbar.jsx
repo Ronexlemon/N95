a2970d314e633589f109f74b0087aaeb6932c04d
RonexLemon <ronexondimu@gmail.com>
2023-08-01 13:55:52 +0300

    presentation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,8 @@ const Navbar = ()=>{
                 <button onClick={()=>{navigate("/solution")}}>Solution</button> 
                 </div>
                 <div className=" hover:text-green-500">
-                <button onClick={()=>{navigate("/research")}}>Research</button> 
+                {/* <button onClick={()=>{navigate("/research")}}>Research</button> */}
+                <a href="https://www.canva.com/design/DAFpi53a9G0/tnvQXIp1OOzXIdvuO1iCbw/edit?utm_content=DAFpi53a9G0&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="blank">Presentation</a> 
                 </div>
              
                 
